ae10348b9ae581e574b07462a61b898aecb2ffc7
Pedro Fernandez Oliver <Pedrofernandezoliver@gmail.com>
2025-03-24 11:29:32 -0300

    Tercer ejer

--- questions.py
+++ questions.py
@@ -10,55 +10,48 @@ questions = [
 ]
 # Respuestas posibles para cada pregunta, en el mismo orden que las preguntas
 answers = [
-    ("size()", "len()", "length()", "count()"),
-    ("3.14", "'42'", "10", "True"),
-    ("input()", "scan()", "read()", "ask()"),
-    (
-        "// Esto es un comentario",
-        "/* Esto es un comentario */",
-        "-- Esto es un comentario",
-        "# Esto es un comentario",
-    ),
-    ("=", "==", "!=", "==="),
+("size()", "len()", "length()", "count()"),
+("3.14", "'42'", "10", "True"),
+("input()", "scan()", "read()", "ask()"),
+(
+"// Esto es un comentario",
+"/* Esto es un comentario */",
+"-- Esto es un comentario",
+"# Esto es un comentario",
+),
+("=", "==", "!=", "==="),
 ]
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
-punt = int(0) 
+punt = 0 # Se inicializa el contador de puntos
 # El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
-
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+for q, a, c in questions_to_ask:
+    print(q)
+    for i, answer in enumerate(a):
         print(f"{i + 1}. {answer}")
-
-    # El usuario tiene 2 intentos para responder correctamente
+    cont = 0 
     for intento in range(2):
-         user_answer = input("Respuesta: ")
-         # Se verifica si la respuesta es válida
-         if user_answer.isdigit():
-             user_answer = int(user_answer)
-             if (user_answer < 5) and (user_answer > 0):
-                 # Se veifica si la respuesta es correcta
-                 user_answer -= 1
-                 if user_answer == correct_answers_index[question_index]:
-                     print("¡Correcto!")
-                     punt += 1
-                     break
-             else:
-                 print("Respuesta no válida.")
-                 sys.exit(1)
-         else:
-              print("Respuesta no válida.")
-              sys.exit(1)
+        cont += 1
+        user_answer = input("Respuesta: ")
+        if user_answer.isdigit():
+            user_answer = int(user_answer)
+            if (user_answer < 5) and (user_answer > 0):
+                if user_answer == c + 1:
+                    print("¡Correcto!")
+                    punt += 1
+                    break
+                else:
+                    if cont == 1:
+                        punt -= 0.5
+            else:
+                print("Respuesta no válida.")
+                sys.exit(1)
+        else:
+                print("Respuesta no válida.")
+                sys.exit(1)
     else:
-         # Si el usuario no responde correctamente después de 2 intentos, se muestra la respuesta correcta
-         print("Incorrecto. La respuesta correcta es:")
-         punt -= 0.5
-         print(answers[question_index] [correct_answers_index[question_index]])
-
-    # Se imprime un blanco al final de la pregunta
-    print()
-print("El puntaje es:", punt)
+        print("Incorrecto. La respuesta correcta es:")
+        print(c + 1)
+        punt -= 0.5
+print("El puntaje del jugador/a es: ", punt)
